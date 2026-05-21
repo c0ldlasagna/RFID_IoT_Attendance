79f7d05f9c8a0e178d8c5bd65ff4af6bfb4ac0c7
c0ldlasagna <erechoum@gmail.com>
2026-05-21 11:55:11 +0700

    Split RGB_LED to separate files

---
+++ firmware/components/Notifications/RGB_LED.cpp
@@ -0,0 +1,19 @@
+#include "RGB_LED.h"
+#include "esp32-hal-ledc.h"
+#include "HardwareSerial.h"
+
+bool RGB_LED::begin(){
+    if(!ledcAttach(r, 10000, 8)){
+        Serial.println("Failed to attach pin R");
+        return false;
+    }
+    if(!ledcAttach(g, 10000, 8)){
+        Serial.println("Failed to attach pin G");
+        return false;        
+    }
+    if(!ledcAttach(b, 10000, 8)){
+        Serial.println("Failed to attach pin B");
+        return false;        
+    }
+    return true;
+}
---
+++ firmware/components/Notifications/include/RGB_LED.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+struct RGB_LED{
+    int r;
+    int g;
+    int b;
+    bool begin();
+};
